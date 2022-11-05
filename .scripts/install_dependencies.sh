be2a6ad73310ee873b8268851712d41181762c61
volodymyr-coax-dev <volodymyr.sen@coaxsoft.com>
2022-11-06 00:07:00 +0200

    masert

---
+++ .scripts/install_dependencies.sh
@@ -0,0 +1,1 @@
+docker pull 267973720966.dkr.ecr.eu-west-3.amazonaws.com/pdp-middle:latest
---
+++ .scripts/start_server.sh
@@ -0,0 +1,1 @@
+docker run -p 3000:3000 --name react-app 267973720966.dkr.ecr.eu-west-3.amazonaws.com/pdp-middle
